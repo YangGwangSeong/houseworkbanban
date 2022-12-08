93b110b919f03a1a3281156d8759ae1f181770a8
YangGwangSeong <rhkdtjd_12@naver.com>
2022-12-08 03:54:06 -0800

    nestjs에 swagger 적용하기

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { BaseAPIDocument } from './swagger.document';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger',app, document);
+  
   await app.listen(5001);
 }
 bootstrap();
